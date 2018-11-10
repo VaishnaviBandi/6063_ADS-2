43dbe3bd67af778edea9588acd1f721266b1c23e
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-11-10 12:50:57 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12/ADS-2_Exam-2/Solution.java
+++ Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-public class Solution {
+public final class Solution {
+	private Solution() {
 
-	public static void main(String[] args) {
+	}
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
 		EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
 		for (int i = 0; i < edges; i++) {
 			String[] edgeValues = scan.nextLine().split(" ");
-			graph.addEdge(new Edge(Integer.parseInt(edgeValues[0]), Integer.parseInt(edgeValues[1]),
-			                       Double.parseDouble(edgeValues[2])));
+			graph.addEdge(new Edge(
+			                  Integer.parseInt(edgeValues[0]),
+			                  Integer.parseInt(edgeValues[1]),
+			                  Double.parseDouble(edgeValues[2])));
 		}
-
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-
-
 		String caseToGo = scan.nextLine();
 		switch (caseToGo) {
 		case "Graph":
@@ -29,15 +27,16 @@ public class Solution {
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			String[] path = scan.nextLine().split(" ");
-            DijkstraSP obj = new DijkstraSP(graph, 
-            	Integer.parseInt(path[0]));
-            if (obj.hasPathTo(Integer.parseInt(path[1]))) {
-                System.out.println(obj.distTo(
-                	Integer.parseInt(path[1])));
-            } else {
-                System.out.println("No Path Found.");
-            }
-            break;
+			DijkstraSP obj = new DijkstraSP(
+			    graph, Integer.parseInt(path[0]));
+			if (obj.hasPathTo(Integer.parseInt(path[1]))) {
+				System.out.println(
+				    obj.distTo(Integer.parseInt(path[1])));
+			} else {
+				System.out.println("No Path Found.");
+			}
+			break;
+
 		case "ViaPaths":
 			// Handle the case of ViaPaths, where three integers are given.
 			// First is the source and second is the via is the one where path should pass throuh.
@@ -45,32 +44,34 @@ public class Solution {
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			String[] viaPath = scan.nextLine().split(" ");
-            DijkstraSP obj1 = new DijkstraSP(
-                graph, Integer.parseInt(viaPath[0]));
-            DijkstraSP obj2 = new DijkstraSP(
-                graph, Integer.parseInt(viaPath[1]));
-            String str = "";
-            if (obj1.hasPathTo(Integer.parseInt(
-                                   viaPath[1])) && obj2.hasPathTo(
-                        Integer.parseInt(viaPath[2]))) {
-                System.out.println(obj1.distTo(
-                                       Integer.parseInt(
-                                           viaPath[1])) + obj2.distTo(
-                                       Integer.parseInt(viaPath[2])));
-                System.out.println(obj1.pathTo(
-                                       Integer.parseInt(
-                                           viaPath[1])) + "" + obj2.pathTo(
-                                       Integer.parseInt(
-                                           viaPath[2])) + viaPath[2]);
-            } else {
-                System.out.println("No Path Found.");
-            }
+			DijkstraSP obj1 = new DijkstraSP(
+			    graph, Integer.parseInt(viaPath[0]));
+			DijkstraSP obj2 = new DijkstraSP(
+			    graph, Integer.parseInt(viaPath[1]));
+			String str = "";
+			if (obj1.hasPathTo(Integer.parseInt(
+			                       viaPath[1])) && obj2.hasPathTo(
+			            Integer.parseInt(viaPath[2]))) {
+				System.out.println(obj1.distTo(
+				                       Integer.parseInt(
+				                           viaPath[1])) + obj2.distTo(
+				                       Integer.parseInt(viaPath[2])));
+				System.out.println(obj1.pathTo(
+				                       Integer.parseInt(
+				                           viaPath[1])) + "" + obj2.pathTo(
+				                       Integer.parseInt(
+				                           viaPath[2])) + viaPath[2]);
+			} else {
+				System.out.println("No Path Found.");
+			}
 
-            break;
+			break;
 
-        default:
-            break;
+		default:
+			break;
 		}
 
 	}
 }
+
+
