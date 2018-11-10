0239f2ba8b13321081c0719e4066a877cf8fa39b
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-11-10 12:42:44 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12/ADS-2_Exam-2/Solution.java
+++ Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -6,14 +6,14 @@ public class Solution {
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
 		EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
-		for(int i = 0;i<edges;i++){
+		for (int i = 0; i < edges; i++) {
 			String[] edgeValues = scan.nextLine().split(" ");
 			graph.addEdge(new Edge(Integer.parseInt(edgeValues[0]), Integer.parseInt(edgeValues[1]),
-				Double.parseDouble(edgeValues[2])));
+			                       Double.parseDouble(edgeValues[2])));
 		}
 
-		// Self loops are not allowed... 
-		// Parallel Edges are allowed... 
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
 		// Take the Graph input here...
 
 
@@ -30,9 +30,9 @@ public class Solution {
 			// Other wise print "No Path Found."
 			String[] path = scan.nextLine().split(" ");
 			DijkstraSP obj = new DijkstraSP(graph, Integer.parseInt(path[0]));
-			if(obj.hasPathTo(Integer.parseInt(path[1]))){
+			if (obj.hasPathTo(Integer.parseInt(path[1]))) {
 				System.out.println(obj.distTo(Integer.parseInt(path[1])));
-			} else{
+			} else {
 				System.out.println("No Path Found.");
 			}
 			break;
@@ -44,11 +44,17 @@ public class Solution {
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			String[] viaPath = scan.nextLine().split(" ");
-			DijkstraSP obj1 = new DijkstraSP(graph, Integer.parseInt(viaPath[0]));
-			DijkstraSP obj2 = new DijkstraSP(graph, Integer.parseInt(viaPath[1]));
-			if(obj1.hasPathTo(Integer.parseInt(viaPath[1]))&&obj2.hasPathTo(Integer.parseInt(viaPath[2]))){
-				System.out.println(obj1.pathTo(Integer.parseInt(viaPath[1]))+" "+obj2.pathTo(Integer.parseInt(viaPath[2])));
-			} else{
+			DijkstraSP obj1 = new DijkstraSP(
+			    graph, Integer.parseInt(viaPath[0]));
+			DijkstraSP obj2 = new DijkstraSP(
+			    graph, Integer.parseInt(viaPath[1]));
+			String str = "";
+			if (obj1.hasPathTo(Integer.parseInt(
+			                       viaPath[1])) && obj2.hasPathTo(
+			            Integer.parseInt(viaPath[2]))) {
+				System.out.println(obj1.distTo(Integer.parseInt(viaPath[1])) + obj2.distTo(Integer.parseInt(viaPath[2])));
+				System.out.println(obj1.pathTo(Integer.parseInt(viaPath[1])) + ""+ obj2.pathTo(Integer.parseInt(viaPath[2])) + viaPath[2]);
+			} else {
 				System.out.println("No Path Found.");
 			}
 			break;
